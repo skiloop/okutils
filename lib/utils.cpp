e44cd32ec9a37cb9bc5b04cc7ab92e4f58d65d7e
skiloop <skiloop@gmail.com>
2023-10-10 18:38:31 +0800

    fix: fix to support content input as bytes

--- lib/utils.cpp
+++ lib/utils.cpp
@@ -1,7 +1,12 @@
 
 #include <boost/python.hpp>
 int64_t mp_append_log(const char * logfile, const char * logs, int len);
+using namespace boost::python;
+int64_t mp_append_log_wrapper(const char * logfile, std::string const &logs){
+    return mp_append_log(logfile, logs.c_str(), logs.length());
+}
+
 BOOST_PYTHON_MODULE (tools) {
-    using namespace boost::python;
-    def("mp_append_log", &mp_append_log, (arg("logfile"), arg("logs") , arg("size")),"append content to logfile");
+//    def("mp_append_log", &mp_append_log, (arg("logfile"), arg("logs") , arg("size")),"append content to logfile");
+    def("mp_append_log", &mp_append_log_wrapper, "append content to logfile");
 }
