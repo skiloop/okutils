a83969b9f11250aba9e00356402eacb0c2fe4a54
skiloop <skiloop@gmail.com>
2024-07-11 22:41:54 +0800

    test: fix tests

--- tests/tools_tests.py
+++ tests/tools_tests.py
@@ -5,8 +5,11 @@ from okutils.tools import mp_append_log
 
 class ToolTestCase(unittest.TestCase):
     def test_mp_append_log(self):
-        size = mp_append_log("/tmp/log.txt", "hello world!")
-        self.assertEqual(size > 0, True)  # add assertion here
+        data = "hello world!"
+        size = mp_append_log("/tmp/log.txt", data)
+        self.assertEqual(size == 0, True)  # add assertion here
+        size = mp_append_log("/tmp/log.txt", data)
+        self.assertEqual(size == len(data), True)  # add assertion here
 
 
 if __name__ == '__main__':
