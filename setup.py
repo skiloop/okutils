c84dc1acb657817c97a6cef0402a968098115f37
skiloop <skiloop@gmail.com>
2024-07-12 03:43:17 +0800

    fix: release version 1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup, Extension
 
 from setuptools import find_packages
 
-tools_ext = Extension('okutils.tools', sources=['lib/funcs.cpp', 'lib/utils.win.cpp'])
+tools_ext = Extension('okutils.tools', sources=['lib/funcs.cpp', 'lib/utils.cpp'])
 
 setup(
     packages=find_packages(exclude=('tests', 'tests.*')),
