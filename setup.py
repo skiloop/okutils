bc09f04ed9ad4e78fcbd4568138bb0d7450073cd
skiloop <skiloop@gmail.com>
2024-07-12 09:53:04 +0800

    build: update version

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@
 
 from setuptools import find_packages, setup, Extension
 
-tools_ext = Extension('okutils.tools', sources=['lib/funcs.cpp', 'lib/utils.cpp'])
+tools_ext = Extension(
+    'okutils.tools',
+    sources=['lib/funcs.cpp', 'lib/utils.cpp'],
+    extra_compile_args=['-std=c++11'],
+)
 
 setup(
     packages=find_packages(exclude=('tests', 'tests.*')),
