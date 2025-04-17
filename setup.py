8e9a9c7cc53016871237aca86ce2db2e09321d8c
Shi Baofeng <skiloop@gmail.com>
2025-04-17 14:15:20 +0800

    fix: fix setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 #!/bin/env python
 # -*- coding:utf8 -*-
 import sys
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 from setuptools import find_packages, setup, Extension
 
 include_dirs, library_dirs = [], []
